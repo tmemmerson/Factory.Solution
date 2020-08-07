5d82ec21f884dd9d51e06b5729a5b5452dbaca13
Tristan Emmerson <tristan@stickerslug.com>
2020-08-07 12:14:35 -0700

    create FactoryContext.cs.

---
+++ Factory/Models/FactoryContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Factory.Models
+{
+  public class FactoryContext : DbContext
+  {
+    public DbSet<Engineer> Engineers { get; set; }
+    public DbSet<Machine> Machines { get; set;}
+    public DbSet<Status> Statuses {get; set;}
+    public DbSet<MachineEngineer> MachineEngineer { get; set; }
+    
+    public FactoryContext(DbContextOptions options) : base(options) {}
+  }
+}
