e07f2ad391c4808c5b1ed8525c5d4bed24f9e188
Tristan Emmerson <tristan@stickerslug.com>
2020-08-07 15:16:17 -0700

    verify code in Status model

--- Factory/Models/Status.cs
+++ Factory/Models/Status.cs
@@ -9,14 +9,14 @@ namespace Factory.Models
 {
   public class Status
   {
+    
     public int StatusId { get; set; }
-
-    [DisplayName("Status Condition")]
-    public string StatusCondition { get; set; }
-
-    [DisplayName("Date Status Updated:")]
-    public DateTime StatusUpdateDate { get; set; }
-
+    [DisplayName("Status Name")]
+    public string StatusName { get; set; }
+    [DisplayName("Date status was Created")]
+    [DataType(DataType.Date)]
+    public DateTime StatusDate { get; set; }
+    public byte[] StatusImage { get; set; }
     public int MachineId { get; set; }
     public Machine Machine { get; set; }
   }
