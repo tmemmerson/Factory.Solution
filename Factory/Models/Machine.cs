f19679bc2396efc1bcb64d07b44042a539fb5e54
Tristan Emmerson <tristan@stickerslug.com>
2020-08-07 09:59:40 -0700

    create Machine model

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+    public class Machine
+    {
+        public Machine()
+        {
+            this.Engineers = new HashSet<MachineEngineer>();
+            this.Statuses = new HashSet<Status>();
+        }
+        public int MachineId { get; set; }
+        public string MachineName { get; set; }
+
+        [DataType(DataType.Date)]
+        public Date DateLastRepaired { get; set; }
+        public ICollection<Status> Statuses { get; set; }
+        public ICollection<MachineEngineer> Engineers { get; set;} 
+    }
+}
