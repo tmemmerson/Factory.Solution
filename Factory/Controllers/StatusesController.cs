8dd577885f67a1d24d7aae0e21841abfd86dacc2
Tristan Emmerson <tristan@stickerslug.com>
2020-08-07 12:23:35 -0700

    create Create for StatusesController following CRUD convention.

--- Factory/Controllers/StatusesController.cs
+++ Factory/Controllers/StatusesController.cs
@@ -21,5 +21,20 @@ namespace Factory.Controllers
       List<Status> model = _db.Statuses.Include(status => status.Machine).ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "MachineType");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Status Status)
+    {
+      _db.Statuses.Add(Status);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }
