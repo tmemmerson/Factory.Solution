cdd5ddd8918bb5fbd53c86f4584e384d3f29963e
Tristan Emmerson <tristan@stickerslug.com>
2020-08-07 13:06:07 -0700

    refactor for build error

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -13,9 +13,7 @@ namespace Factory.Models
         }
         public int MachineId { get; set; }
         public string MachineType { get; set; }
-
-        [DataType(DataType.Date)]
-        public Date DateLastRepaired { get; set; }
+        public DateTime DateLastRepaired { get; set; }
         public ICollection<Status> Statuses { get; set; }
         public ICollection<MachineEngineer> Engineers { get; set;} 
     }
