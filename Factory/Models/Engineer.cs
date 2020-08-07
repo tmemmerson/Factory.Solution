b7209776a701833de5c80d7a9107a6763cab8df1
Tristan Emmerson <tristan@stickerslug.com>
2020-08-07 11:08:16 -0700

    create Engineer model

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+    public class Engineer
+    {
+      public Engineer()
+      {
+          this.Machines = new HashSet<MachineEngineer>();
+      }
+
+      public int EngineerId { get; set; }
+      public string EngineerName { get; set; }
+      public string EngineerDescription { get; set; }
+      
+      [DataType(DataType.Date)]
+      public Date EngineerHireDate { get; set; }
+      
+      public virtual ICollection<MachineEngineer> Machines { get; set; }
+    }
+}
